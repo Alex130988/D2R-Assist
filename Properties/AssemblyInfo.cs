c0fe19488f89fa794aa9eb341221255850f88f46
Alexander Kalusche <alexanderkalusche@t-online.de>
2021-10-29 16:46:50 +0200

    Version change 3.0.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,21 +1,29 @@
-﻿/**
- *   Copyright (C) 2021 okaygo
- *
- *   https://github.com/misterokaygo/D2RAssist/
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- **/
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+
+[assembly: AssemblyTitle("D2R-Assist")]
+[assembly: AssemblyDescription("D2R-Assist")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: ComVisible(true)]
+/**
+*   Copyright (C) 2021 okaygo
+*
+*   https://github.com/misterokaygo/D2RAssist/
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
 
 #if DEBUG
 using System.Reflection;
